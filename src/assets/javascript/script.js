3b9ea68dfaebcb29c92809df46ee7e5ac0af4065
Ibrahim Olayinka <raimiibrahim44@gmail.com>
2021-05-13 15:02:30 +0100

    Fixed the transition pop up when reducing the width and hamburger menu close up when link is clicked

--- src/assets/javascript/script.js
+++ src/assets/javascript/script.js
@@ -2,10 +2,21 @@
 const navToggle = () => {
     const burger = document.querySelector('.burger');
     const navList = document.querySelector('.nav-list');
+    const navLinks = document.querySelectorAll('.nav-list li');
 
     burger.addEventListener('click', () => {
+        // Toggle Navbar
         navList.classList.toggle('active');
         burger.classList.toggle('toggle');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+            }
+        });
     });
 }
 
